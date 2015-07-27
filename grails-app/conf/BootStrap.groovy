630924af593c29398e898c7d70e3ad6b991ab168
Ika Belerma <ika.belerma@gmail.com>
2015-07-28 00:19:43 +0800

    On runtime, adds test data via Bootstrap.
    Signed-off-by: Ika Belerma <ika.belerma@gmail.com>

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,19 @@
+import mmxv.webapp.Criteria
+import mmxv.webapp.Employee
+import mmxv.webapp.Team
+
 class BootStrap {
 
     def init = { servletContext ->
+		new Team (name: "Power Express Optimization").save()
+		new Criteria (name: "Voice of the Customer", maxPoints : 100).save()
+		new Employee (
+			 uid: "UEXB524",
+			 firstName: "Ika", 
+			 lastName: "Belerma", 
+			 emailAddress: "ebelerma@carlsonwagonlit.com", 
+			 mobileNumber: "639989701909", 
+			 isApprover: false).save()
     }
     def destroy = {
     }
