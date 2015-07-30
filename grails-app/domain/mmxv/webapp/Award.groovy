e09d6f96b9bc23f3ecaaf3458b7c8271aa6c6c6f
Ika Belerma <ika.belerma@gmail.com>
2015-07-30 22:39:52 +0800

    I suck at this merging stuff.
    Removed remaining conflict tags.
    
    Signed-off-by: Ika Belerma <ika.belerma@gmail.com>

--- grails-app/domain/mmxv/webapp/Award.groovy
+++ grails-app/domain/mmxv/webapp/Award.groovy
@@ -1,22 +1,11 @@
 package mmxv.webapp
 
 class Award {
-<<<<<<< HEAD
-	Nomination nomination
-	static belongsTo = [nominee : Employee]
-	Integer pointsAwarded
-	String remarks
-
-    static constraints = {
-		pointsAwarded unique:false 
-		remarks blank: true,  maxSize : 1000
-=======
 	Integer pointsAwarded
 	String remarks
 	
     static constraints = {
 		pointsAwarded blank: false
 		remarks blank: true 
->>>>>>> awards
     }
 }
